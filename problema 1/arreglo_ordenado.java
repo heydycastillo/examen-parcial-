9e4894aa8a6cf71f9b22f28007e8b4b5a0fcf568
heydy <hcastilloa@ulasalle.edu.pe>
2026-05-18 20:39:46 -0500

    arreglo de n

--- problema 1/arreglo_ordenado.java
+++ problema 1/arreglo_ordenado.java
@@ -1,20 +1,20 @@
 public class arreglo_ordenado {
     
-    public static boolean identificador(int[] arr, int i,int n) {
-        if (i == n - 1) {
+    public static boolean identificador(int[] array, int i,int n) {
+        if (i == n ) {
             return true;
         }
-        if (arr[i] > arr[i + 1]) {
+        if (array[i] > array[i + 1]) {
             return false;
         }
-        return identificador(arr, i + 1,n);
+        return identificador(array, i + 1,n);
     }
     
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 5, 8};
-        int n =5;
+        int n =4;
         int[] array2 = {1, 4,2,8};
-        int x =4;
+        int x =3;
         System.out.println("arreglo 1:");
         System.out.println(identificador(array, 0,n));
         System.out.println("arreglo2:");
