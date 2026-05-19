120d5061769a301427bfb0bbe8060a32701e3af0
heydy <hcastilloa@ulasalle.edu.pe>
2026-05-18 19:48:21 -0500

    no se subio en el anterior

---
+++ problema 2/problema2.java
@@ -0,0 +1,14 @@
+public class problema2 {
+
+     static int segundoMayor(int[] arr, int i, int j) {
+     }
+
+    public static void main(String[] args) {
+
+        int[] arr = {8, 3, 10, 5, 7};
+        int n = 4;
+
+        System.out.println(segundoMayor(arr,0, n)); 
+
+    }
+}
