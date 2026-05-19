4cda099e2779f040861789bcdac5be0394246dcc
heydy <hcastilloa@ulasalle.edu.pe>
2026-05-18 20:17:28 -0500

    agrego caso base y llamadas recursivas para dividir el arreglo

--- problema 2/problema2.java
+++ problema 2/problema2.java
@@ -1,14 +1,27 @@
 public class problema2 {
 
-     static int segundoMayor(int[] arr, int i, int j) {
-     }
+    static int segundoMayor(int[] arr, int i, int n) {
+        if (n == i + 1) {
+            if (arr[i] > arr[n]) {
+                return arr[n];
+            } else {
+                return arr[i];
+            }
+        }
+        int mitad= (i + n) / 2;
+
+        int izq = segundoMayor(arr, i, mitad);
+        int der = segundoMayor(arr, mitad + 1, n);
+
+        return 0;
+    }
 
     public static void main(String[] args) {
 
         int[] arr = {8, 3, 10, 5, 7};
-        int n = 4;
-
-        System.out.println(segundoMayor(arr,0, n)); 
+        int n=4;
+        int segundomayor = segundoMayor(arr, 0, n);
+        System.out.println(segundomayor);
 
     }
 }
